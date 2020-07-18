236b8ef702bd40100b3e22c566044cdc86c032cb
somsomdah <somdah98@gmail.com>
2020-07-18 15:57:02 +0900

    first commit

---
+++ app/src/main/java/com/example/note/CheckingActivity.java
@@ -0,0 +1,60 @@
+package com.example.note;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CheckingActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checking);
+
+        Intent intent=getIntent();
+        String feeling=intent.getStringExtra("feeling");
+
+        TextView textView=findViewById(R.id.textView3);
+        ImageView imageView=findViewById(R.id.imageView2);
+        Button noButton =findViewById(R.id.button2);
+        Button yesButton=findViewById(R.id.button3);
+
+        if (feeling.equals("angry")) {
+            imageView.setImageResource(R.drawable.angry);
+            textView.setText("Are You Ready to \n Write Your Musical Anger?");
+        }
+
+        /////
+        ///감정별 if문 작성
+        ///musical calmness, musical happiness, musical sadness 정도?
+        /////
+
+
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(CheckingActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(CheckingActivity.this,RecordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/note/FirstScreenActivity.java
@@ -0,0 +1,30 @@
+package com.example.note;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class FirstScreenActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_screen);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(FirstScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
