c0034dbb7a1908bd986e53c04837af72a2fdfde7
somsomdah <somdah98@gmail.com>
2020-07-24 15:51:48 +0900

    add checking modal

--- app/src/main/java/com/example/note/MainActivity.java
+++ app/src/main/java/com/example/note/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.note;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -10,11 +11,14 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.NoCopySpan;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -24,10 +28,18 @@ public class MainActivity extends AppCompatActivity {
     private ImageView neutral;
     private ImageView sad;
     private  ImageView angry;
+
     private ImageView main;
     private  ImageView calendar;
     private ImageView stars;
     private ImageView settings;
+
+    private ConstraintLayout checkingModal;
+    private TextView checkingModalText;
+    private Button goBack;
+    private Button continue_;
+    private ConstraintLayout wholeScreen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,34 +50,59 @@ public class MainActivity extends AppCompatActivity {
         sad=findViewById(R.id.sad);
         angry=findViewById(R.id.angry);
 
+        checkingModal=findViewById(R.id.checkingModal);
+        checkingModalText=findViewById(R.id.checkingModalText);
+        goBack=findViewById(R.id.goBack);
+        continue_=findViewById(R.id.continue_);
+
+        wholeScreen=findViewById(R.id.wholeScreen);
+
+
 
         happy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 여기 채우기
             }
         });
 
         neutral.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // 여기 채우기
             }
         });
         sad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // 여기 채우기
             }
         });
 
         angry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,CheckingActivity.class);
-                intent.putExtra("feeling","angry");
+                checkingModal.setVisibility(View.VISIBLE);
+                checkingModalText.setText("write an angry music" );
+            }
+        });
+
+
+
+        continue_.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, RecordActivity.class);
                 startActivity(intent);
             }
         });
+
+        goBack.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkingModal.setVisibility(View.GONE);
+            }
+        }));
 
         main=findViewById(R.id.main);
         calendar=findViewById(R.id.calendar);
@@ -76,7 +113,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                overridePendingTransition(0,0); //0 for no animation
                 startActivity(intent);
+
+
             }
         });
 
@@ -84,8 +125,12 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+
                 Intent intent=new Intent(getApplicationContext(),CalendarActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                overridePendingTransition(0,0); //0 for no animation
                 startActivity(intent);
+
 
             }
         });
@@ -95,7 +140,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(getApplicationContext(),StarsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                overridePendingTransition(0,0); //0 for no animation
                 startActivity(intent);
+
 
             }
         });
@@ -105,7 +153,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(getApplicationContext(),SettingsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                overridePendingTransition(0,0); //0 for no animation
                 startActivity(intent);
+
 
             }
         });
