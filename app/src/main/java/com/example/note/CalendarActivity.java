818848c47d7ebd08e6e6b078c5960bbcffb772f8
vivi-github <vivi-github@users.noreply.github.com>
2020-07-30 16:03:44 +0900

    200730
    changed version v7 to androidx, inserted calendar, etc

--- app/src/main/java/com/example/note/CalendarActivity.java
+++ app/src/main/java/com/example/note/CalendarActivity.java
@@ -7,12 +7,50 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class CalendarActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+
+
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateLongClickListener;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
+import org.threeten.bp.format.DateTimeFormatter;
+
+import java.util.List;
+
+
+public class CalendarActivity extends AppCompatActivity
+        implements OnDateSelectedListener, OnMonthChangedListener, OnDateLongClickListener {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("EEE, d MMM yyyy");
+    public static final CharSequence[] ITEMS =
+            new CharSequence[] { "EDIT", "SINGLE", "MULTIPLE", "RANGE" };
+
+    @BindView(R.id.calendarView)
+    MaterialCalendarView widget;
+
+    @BindView(R.id.textView)
+    TextView textView;
 
     private ImageView main;
     private  ImageView calendar;
     private ImageView stars;
     private ImageView settings;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +115,53 @@ public class CalendarActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public void onDateSelected(
+            @NonNull MaterialCalendarView widget,
+            @NonNull CalendarDay date,
+            boolean selected) {
+        textView.setText(selected ? FORMATTER.format(date.getDate()) : "No Selection");
+    }
+
+    @Override
+    public void onDateLongClick(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date) {
+        final String text = String.format("%s is available", FORMATTER.format(date.getDate()));
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+        //noinspection ConstantConditions
+        getSupportActionBar().setTitle(FORMATTER.format(date.getDate()));
+    }
+
+    @OnCheckedChanged(R.id.calendar_mode)
+    void onCalendarModeChanged(boolean checked) {
+        final CalendarMode mode = checked ? CalendarMode.WEEKS : CalendarMode.MONTHS;
+        widget.state().edit().setCalendarDisplayMode(mode).commit();
+    }
+
+//    @OnClick(R.id.button_selection_mode) void onChangeSelectionMode() {
+//        new AlertDialog.Builder(this)
+//                .setTitle("Selection Mode")
+//                .setSingleChoiceItems(ITEMS, widget.getSelectionMode(), (dialog, which) -> {
+//                    widget.setSelectionMode(which);
+//                    dialog.dismiss();
+//                })
+//                .show();
+//    }
+
+    @OnClick(R.id.get_selected_dates) public void getSelectedDateClick(final View v) {
+        final List<CalendarDay> selectedDates = widget.getSelectedDates();
+        if (!selectedDates.isEmpty()) {
+            Toast.makeText(this, selectedDates.toString(), Toast.LENGTH_SHORT).show();
+            Log.e("GettersActivity", selectedDates.toString());
+        } else {
+            Toast.makeText(this, "No Selection", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
 
 }
